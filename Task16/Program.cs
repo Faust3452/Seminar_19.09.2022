554263311c9c6656b7c77cb7c8541ead6495c926
alexandr <chochumakov_alek@mail.ru>
2022-09-19 22:55:39 +0300

    Making Task16 better

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -10,6 +10,7 @@ void Square(int num1, int num2)
     if (num1 * num1 == num2) Console.WriteLine($"Число {num1} является квадратом {num2}");
     else Console.WriteLine($"Число {num1} не является квадратом {num2}");
 }
+
 Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
