ebce674c238498aeb84ca0d0b79750b37c862c6e
alexandr <chochumakov_alek@mail.ru>
2022-09-19 22:54:18 +0300

    Added DZ_Task10

---
+++ DZ_Task10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+bool IsThreeDigit(int num)
+{
+    return (((num > 99) && (num < 1000)) || (num < -99) && (num > -1000));
+}
+
+void ShowSecondDigit(int num)
+{
+    int result = ((num / 10) % 10);
+    if (result < 0) result = -result;
+    Console.WriteLine($"Вторая цифра числа {num} => {result}");
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+bool checkTG = IsThreeDigit(number);
+if (checkTG == true)
+{
+    ShowSecondDigit(number);
+}
+else Console.WriteLine("Вы ввели не трехзначное число :( \n Попробуйте заново");
